e42e240954fce4d7694795cd5231fd02dff98709
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2023-01-09 13:48:38 +0100

    reamoved unused code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
